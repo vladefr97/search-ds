d7664dbae22fcf1402843c50ced98b89b071e063
Nechaev_Mikhail <mikhail.nechaev@odnoklassniki.ru>
2016-12-24 19:24:16 +0300

    init

---
+++ src/ru/mail/polis/Test.java
@@ -0,0 +1,65 @@
+package ru.mail.polis;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by Nechaev Mikhail
+ * Since 24/12/16.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        new Test().run(0b1111);
+    }
+
+    private final int MASK = 0b1111;
+
+    Comparator<Integer> EVEN_FIRST = (v1, v2) -> {
+        // Even first
+        final int c = Integer.compare(v1 % 2, v2 % 2);
+        return c != 0 ? c : Integer.compare(v1, v2);
+    };
+
+    private void run(int idx) {
+        if (test(idx,0)) testTree("avl", new AVLTree<>());
+        if (test(idx,1)) testTree("avlE", new AVLTree<>(EVEN_FIRST));
+        if (test(idx,2)) testTree("RB", new RedBlackTree<>());
+        if (test(idx,3)) testTree("RBE", new RedBlackTree<>(EVEN_FIRST));
+    }
+
+    private boolean test(int idx, int shift) {
+        return (((MASK >> shift) & 1) == ((idx >> shift) & 1));
+    }
+
+    private void testTree(String name, ISortedSet<Integer> set) {
+        System.err.println("-------------------- " + name + " -------------------");
+        try {
+            try {
+                System.out.println(set.last());
+                System.out.println(set.first());
+            } catch (NoSuchElementException e) {
+                System.out.println("OK");
+            }
+            for (int i = 0; i < 15; i++) {
+                set.add(i);
+            }
+            System.out.println(set.inorderTraverse());
+            System.out.println(set.last());
+            System.out.println(set.first());
+            for (int i = 15; i < 30; i++) {
+                set.add(i);
+                if (i % 2 == 0) {
+                    set.remove(i);
+                }
+            }
+            System.out.println(set);
+            System.out.println(set.contains(-1));
+            System.out.println(set.contains(101));
+            System.out.println(set.contains(15));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.err.println("^^^^^^^^^^^^^^^^^^^^^ " + name + " ^^^^^^^^^^^^^^^^^^^");
+    }
+}
