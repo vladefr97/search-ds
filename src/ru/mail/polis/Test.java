275c5240767f862ea3017e5f144f78877abfda47
Nechaev_Mikhail <mikhail.nechaev@odnoklassniki.ru>
2016-12-24 19:34:06 +0300

    assert

--- src/ru/mail/polis/Test.java
+++ src/ru/mail/polis/Test.java
@@ -10,7 +10,7 @@ import java.util.NoSuchElementException;
 public class Test {
 
     public static void main(String[] args) {
-        new Test().run(0b1111);
+        new Test().run(0b0001);
     }
 
     private final int MASK = 0b1111;
@@ -35,28 +35,44 @@ public class Test {
     private void testTree(String name, ISortedSet<Integer> set) {
         System.err.println("-------------------- " + name + " -------------------");
         try {
+            assert set.size() == 0;
+            assert set.isEmpty();
             try {
                 System.out.println(set.last());
                 System.out.println(set.first());
+                assert false;
             } catch (NoSuchElementException e) {
                 System.out.println("OK");
             }
             for (int i = 0; i < 15; i++) {
-                set.add(i);
+                assert set.contains(i) != set.add(i);
+                assert set.contains(i) != set.add(i);
             }
             System.out.println(set.inorderTraverse());
-            System.out.println(set.last());
-            System.out.println(set.first());
+            assert set.size() == 15;
+            assert !set.isEmpty();
+            assert set.last() == 14;
+            assert set.first() == 0;
             for (int i = 15; i < 30; i++) {
                 set.add(i);
+                assert set.contains(i) != set.add(i);
+                assert set.contains(i) != set.add(i);
                 if (i % 2 == 0) {
                     set.remove(i);
+                    assert set.contains(i) == set.remove(i);
+                    assert set.contains(i) == set.remove(i);
                 }
             }
-            System.out.println(set);
-            System.out.println(set.contains(-1));
-            System.out.println(set.contains(101));
-            System.out.println(set.contains(15));
+            System.out.println(set.inorderTraverse());
+            assert set.size() == 22;
+            assert !set.isEmpty();
+            assert !set.contains(-1);
+            assert !set.contains(101);
+            assert set.contains(15);
+            for (int i = 100; i >= 0; i++) {
+                assert set.contains(i) == set.remove(i);
+                assert set.contains(i) == set.remove(i);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
